552c7aec3a038655b1655c544a6da9d3a538a6dc
Thiago Gabriel Tahim <121395111+tahim06@users.noreply.github.com>
2024-03-14 11:28:44 +0000

    correção no preenchimento do vetor

--- Exercicios/ex10.c
+++ Exercicios/ex10.c
@@ -2,16 +2,16 @@
 #include <stdlib.h>
 
 int main(){
-    int qtd = 15;
+    int qtd = 5;
     int v[qtd],atual;
     for(int i = 0; i < qtd; i++){
-        scanf("%d",&v[qtd]);
+        scanf("%d",&v[i]);
     }
     
     for(int i = 1; i < qtd; i++){
         atual = v[i];
         int j = i-1;
-        while(j>=0 && atual < v[j]){
+        while(j >= 0 && atual < v[j]){
             v[j+1] = v[j];
             j = j-1;
         } 

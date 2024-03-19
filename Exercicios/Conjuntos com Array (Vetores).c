27f148d5e5cbfcb7e7be986b628cbb7472d77e3a
Thiago Gabriel Tahim <121395111+tahim06@users.noreply.github.com>
2024-03-19 23:10:16 +0000

    Correção nos testes de igualdade

--- Exercicios/Conjuntos com Array (Vetores).c
+++ Exercicios/Conjuntos com Array (Vetores).c
@@ -15,25 +15,40 @@ int main(){
     for(int i = 0; i < size1; i++){
         printf("Digite o %dº valor do Vetor 1: ", i+1);
         scanf("%d",&vetor1[i]);
-        for(int j = 0; j < i; j++){
-            if(vetor1[j] == vetor1[i]){
-                printf("Este valor já foi digitado, por favor digite outro valor para a %dª posição do Vetor1: ", i+1);
-                scanf("%d",&vetor1[i]);
+        printf("\n");
+
+        if(i != 0){
+            for(int j = 0; j < i; j++){
+                while(vetor1[j] == vetor1[i]){
+                    printf("Este valor já foi digitado, por favor digite outro valor para a %dª posição do Vetor1: ", i+1);
+                    scanf("%d",&vetor1[i]);
+                    printf("\n");
+                }
+   
             }
+
         }
+        
     }
 
     for(int i = 0; i < size2; i++){
         printf("Digite o %dº valor do Vetor 2: ", i+1);
         scanf("%d",&vetor2[i]);
-        for(int j = 0; j < i; j++){
-            if(vetor2[j] == vetor2[i]){
-                printf("Este valor já foi digitado, por favor digite outro valor para a %dª posição do Vetor2: ", i+1);
-                scanf("%d",&vetor2[i]);
-            }
-        }
-    }
+        printf("\n");
 
+        if(i != 0){
+            for(int j = 0; j < i; j++){
+                while(vetor2[j] == vetor2[i]){
+                    printf("Este valor já foi digitado, por favor digite outro valor para a %dª posição do Vetor1: ", i+1);
+                    scanf("%d",&vetor2[i]);
+                    printf("\n");
+                }
+   
+            }
+
+        }
+        
+    }
     
 
 

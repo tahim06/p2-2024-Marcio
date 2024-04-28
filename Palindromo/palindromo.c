326e87c6c7613ca95fdf9dab2f4dfd0300fbbf4a
Thiago Gabriel Tahim <121395111+tahim06@users.noreply.github.com>
2024-04-28 17:57:25 +0000

    Retirar espaços

--- Palindromo/palindromo.c
+++ Palindromo/palindromo.c
@@ -1,13 +1,30 @@
 #include <stdio.h>
-#include <ctype.h>
+#include <string.h>
+#define TAM 51
 
-int main(){
+void removerEspaco(char frase[]);
 
+int main() {
+    char frase[TAM];
+    do {
+        printf("Digite uma Frase com até %d caracteres: ", TAM - 1);
+        scanf(" %[^\n]", frase);
+    } while (strlen(frase) < 1 || strlen(frase) >= TAM);
+
+    removerEspaco(frase);
+
+    puts(frase);
 
     return 0;
 }
 
-char removerEspaço(){
-
-    
+void removerEspaco(char frase[]) {
+    int count = 0;
+    for (int i = 0; frase[i] != '\0'; i++) {
+        if (frase[i] != ' ') {
+            frase[count++] = frase[i];
+        }
+    }
+    frase[count] = '\0';
 }
+

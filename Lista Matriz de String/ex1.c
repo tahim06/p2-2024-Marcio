0925e7b146c056f92bf9cf9847ba0b42def5b827
Thiago Gabriel Tahim <121395111+tahim06@users.noreply.github.com>
2024-05-09 11:59:13 +0000

    recebe valores e faz a media

---
+++ Lista Matriz de String/ex1.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#define LINHAS 20
+#define COLUNAS 80
+
+int main(){
+    char nomeAluno[LINHAS][COLUNAS];
+    double primNota[LINHAS],segNota[LINHAS],media[LINHAS];
+
+    for(int i = 0 ; i < LINHAS; i++){
+        printf("Informe o nome do %d aluno: ", i + 1);
+        fgets(nomeAluno[i],COLUNAS,stdin);
+        
+        printf("Informe a primeira nota do aluno %s: ", nomeAluno[i]);
+        scanf("%lf",&primNota[i]);
+        
+        printf("Informe a segunda nota do aluno %s: ", nomeAluno[i]);
+        scanf("%lf",&segNota[i]);
+    }
+
+    for(int i = 0; i < LINHAS; i++){
+        media[i] = ((primNota[i] * 2) + (segNota[i] * 3)) / 5;
+    }
+
+    
+
+
+    return 0;
+}

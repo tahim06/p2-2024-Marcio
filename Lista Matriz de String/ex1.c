f5a1472d88c8536b6ccbb3550d1da8f42e257b8a
Thiago Gabriel Tahim <121395111+tahim06@users.noreply.github.com>
2024-05-13 02:09:43 +0000

    inicio

--- Lista Matriz de String/ex1.c
+++ Lista Matriz de String/ex1.c
@@ -2,35 +2,46 @@
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
-#define LINHAS 20
+#define ALUNOS 2
 #define COLUNAS 80
 
-int main(){
-    char nomeAluno[LINHAS][COLUNAS];
-    double primNota[LINHAS],segNota[LINHAS],media[LINHAS];
+int passou(double media);
 
-    for(int i = 0 ; i < LINHAS; i++){
-        printf("Informe o nome do %d aluno: ", i + 1);
-        fgets(nomeAluno[i],COLUNAS,stdin);
-        
+int main(){
+    char nomeAluno[ALUNOS][COLUNAS];
+    double primNota[ALUNOS],segNota[ALUNOS],media[ALUNOS];
+
+    for(int i = 0 ; i < ALUNOS; i++){
+        printf("Informe o nome do %d aluno:\n", i + 1);
+        fgets(nomeAluno[i], COLUNAS, stdin);
+        int tam = strlen(nomeAluno[i]);
+        if (nomeAluno[i][tam - 1] == '\n') 
+            nomeAluno[i][tam - 1] = '\0';  
+
         printf("Informe a primeira nota do aluno %s: ", nomeAluno[i]);
-        scanf("%lf",&primNota[i]);
+        scanf("%lf", &primNota[i]);
         
         printf("Informe a segunda nota do aluno %s: ", nomeAluno[i]);
-        scanf("%lf",&segNota[i]);
+        scanf("%lf", &segNota[i]);
+        getchar(); 
     }
 
-    for(int i = 0; i < LINHAS; i++){
+    for(int i = 0; i < ALUNOS; i++){
         media[i] = ((primNota[i] * 2) + (segNota[i] * 3)) / 5;
-    }
-
-    for(int i = 0; i < LINHAS; i++){
-        int passado = 0;
-        if(media[i] >= 7){
-            
+        if(passou(media[i])){
+            printf("O aluno %s obteve média %lf e foi Aprovado.\n", nomeAluno[i], media[i]);
+        }else{
+            printf("O aluno %s obteve média %lf e foi Reprovado.\n", nomeAluno[i], media[i]);
         }
     }
 
-
     return 0;
+}
+
+int passou(double media){
+    int aprovado = 0;
+    if(media >= 7){
+        aprovado = 1;
+    }
+    return aprovado;       
 }

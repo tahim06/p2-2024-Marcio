6001bd1046878fec7c1dd7b3dcb681a47cd80bd2
Thiago Gabriel Tahim <121395111+tahim06@users.noreply.github.com>
2024-05-25 14:43:48 +0000

    Função para encontrar os estados com o maior e menor numero de acidentes

--- Struct/ex1.c
+++ Struct/ex1.c
@@ -11,30 +11,46 @@ struct infoEstados{
     int qtdAcidentes;
 };
 
-void solicitarDados(struct infoEstados estado[], int qtdEstados,int tamTexto);
+void solicitarDados(struct infoEstados *estado[], int qtdEstados,int tamTexto);
+void maior_menor_qtdAcidentes(struct infoEstados *estado[], int qtdEstados,int *maior, int *menor);
+
 
 int main(){
     struct infoEstados estados[ESTADOS];
     solicitarDados(estados,ESTADOS,TAM_TEXTO);
 
+    int indice_maior, indice_menor;
+
     return 0;
 }
 
-void solicitarDados(struct infoEstados estado[], int qtdEstados,int tamTexto){
+
+void solicitarDados(struct infoEstados *estado[], int qtdEstados,int tamTexto){
     for(int i = 0; i < qtdEstados;i++){
         printf("Informe o nome do %d estado:\n ", i+1);
-        fgets(estado[i].nome,tamTexto,stdin);
+        fgets(estado[i]->nome,tamTexto,stdin);
         
-        printf("Informe quantos veículos circulam em %s:\n ", estado[i].nome);
-        scanf("%d",&estado[i].qtdVeiculos);
+        printf("Informe quantos veículos circulam em %s:\n ", estado[i]->nome);
+        scanf("%d",&estado[i]->qtdVeiculos);
 
-        printf("Informe quantos acidentes de trânsito tiveram em %s: ", estado[i].nome);
-        scanf("%d",&estado[i].qtdAcidentes);
+        printf("Informe quantos acidentes de trânsito tiveram em %s: ", estado[i]->nome);
+        scanf("%d",&estado[i]->qtdAcidentes);
         
         while(getchar() != '\n');
     }
 } 
 
-void maior_menor_qtdAcidentes(struct infoEstados *estado[], int qtdEstados){
-
+void maior_menor_qtdAcidentes(struct infoEstados *estado[], int qtdEstados,int *maior, int *menor){
+    *maior = 0;
+    *menor = 0;
+    
+    for(int i = 0; i < qtdEstados;i++){
+        if(estado[i]->qtdAcidentes > estado[*maior]->qtdAcidentes){
+            *maior = i;
+        }
+        else if(estado[i]->qtdAcidentes < estado[*menor]->qtdAcidentes){
+            *menor = i;
+        }
+    }
 }
+

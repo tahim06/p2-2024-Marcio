6182ad716ce807402240cd698d9137ad6d1c1182
Thiago Gabriel Tahim <121395111+tahim06@users.noreply.github.com>
2024-06-06 00:09:34 +0000

    Consultar Usuario adicionado

--- Projeto Final/projeto.c
+++ Projeto Final/projeto.c
@@ -12,13 +12,15 @@
 #define TAM_MUSICAS 100
 #define TAM_MAX_USUARIOS 100
 
-struct Musica {
+struct Musica
+{
     int codigo;              // autoincremento
     char titulo[TAM_TITULO]; // título da música
     char artista[TAM_NOME];  // nome do artista que canta esta música
 };
 
-struct Playlist {
+struct Playlist
+{
     int codigo;               // autoincremento
     int codUsuario;           // o código do usuário que criou a playlist
     char titulo[TAM_TITULO];  // título desta playlist
@@ -26,7 +28,8 @@ struct Playlist {
     int musicas[TAM_MUSICAS]; // vetor contendo os códigos das músicas
 };
 
-struct Usuario {
+struct Usuario
+{
     int codigo; // autoincremento
     char nome[TAM_NOME];
     char login[TAM_LOGIN];
@@ -44,104 +47,156 @@ void cadastro(struct Usuario usuario[], int *contUsuarios, int *posicao);
 void listarUsuarios(struct Usuario usuario[], int qtdUsuarios);
 void listarMusicas(struct Musica musicas[], int qtdMusicas);
 void listarPlaylists(struct Usuario usuario[], int qtdUsuarios, struct Musica musicas[], int qtdMusicas);
-char* buscarTituloMusica(struct Musica musicas[], int qtdMusicas, int codigo);
+char *buscarTituloMusica(struct Musica musicas[], int qtdMusicas, int codigo);
+void cadastrarMusicas(struct Musica musicas[], int *qtdMusicas);
+void consultarUsuario(struct Usuario usuario[], int qtdUsuarios);
 
-int main() {
+int main()
+{
     int tipoUsuario, contUsuarios = 0;
     int posicao_usuario = -1;
     struct Usuario usuario[TAM_MAX_USUARIOS];
     struct Musica musicas[TAM_MUSICAS];
     int qtdMusicas = 0;
     int logadoAdm = 0, logadoUsuario = 0;
-    int sair = 0;  
+    int sair = 0;
 
-    while (!sair) {
-        do {
+    while (!sair)
+    {
+        do
+        {
             printf("Como Deseja Logar?\n 1- Administrador\n 2- Usuário\n 3- Encerrar Programa\n");
             scanf("%d", &tipoUsuario);
-            while (getchar() != '\n');
+            while (getchar() != '\n')
+                ;
         } while (tipoUsuario < 1 || tipoUsuario > 3);
 
-        if (tipoUsuario == 3) {
+        if (tipoUsuario == 3)
+        {
             sair = 1;
             continue;
         }
 
-        if (tipoUsuario == 1) {
+        if (tipoUsuario == 1)
+        {
             int tentarNovamenteADM = 1;
-            while (tentarNovamenteADM) {
-                if (loginAdministrador()) {
+            while (tentarNovamenteADM)
+            {
+                if (loginAdministrador())
+                {
                     logadoAdm = 1;
-                    tentarNovamenteADM = 0; // Login bem-sucedido, sair do loop
-                } else {
-                    printf("O que Deseja?\n 1- Tentar Novamente\n 2- Mudar opção de Login\n");
-                    scanf("%d", &tentarNovamenteADM);
-                    while (getchar() != '\n');
-                    if (tentarNovamenteADM == 2) {
+                    tentarNovamenteADM = 0;
+                }
+                else
+                {
+                    do
+                    {
+                        printf("O que Deseja?\n 1- Tentar Novamente\n 2- Mudar opção de Login\n");
+                        scanf("%d", &tentarNovamenteADM);
+                        while (getchar() != '\n')
+                            ;
+                    } while (tentarNovamenteADM < 1 || tentarNovamenteADM > 2);
+
+                    if (tentarNovamenteADM == 2)
+                    {
                         break;
                     }
                 }
             }
-        } else {
+        }
+        else
+        {
             int opcao;
             int tentarNovamente;
 
-            do {
+            do
+            {
                 printf("Já possui um cadastro ou deseja cadastrar?\n 1- Logar\n 2- Cadastrar\n");
                 scanf("%d", &opcao);
-                while (getchar() != '\n');
+                while (getchar() != '\n')
+                    ;
             } while (opcao < 1 || opcao > 2);
 
-            if (opcao == 1) {
-                while (1) {
-                    if (loginUsuario(usuario, &contUsuarios, &posicao_usuario)) {
+            if (opcao == 1)
+            {
+                while (1)
+                {
+                    if (loginUsuario(usuario, &contUsuarios, &posicao_usuario))
+                    {
                         printf("Login Bem Sucedido !!\n");
                         logadoUsuario = 1;
                         break;
-                    } else {
+                    }
+                    else
+                    {
                         printf("Login ou Senha inválida!!\n\n");
-                        do {
+                        do
+                        {
                             printf("O que Deseja?\n 1- Tentar Novamente\n 2- Cadastrar Novo Usuário\n");
                             scanf("%d", &tentarNovamente);
-                            while (getchar() != '\n');
+                            while (getchar() != '\n')
+                                ;
                         } while (tentarNovamente < 1 || tentarNovamente > 2);
 
-                        if (tentarNovamente == 1) {
+                        if (tentarNovamente == 1)
+                        {
                             continue;
-                        } else {
+                        }
+                        else
+                        {
                             cadastro(usuario, &contUsuarios, &posicao_usuario);
                             logadoUsuario = 1;
                             break;
                         }
                     }
                 }
-            } else {
+            }
+            else
+            {
                 cadastro(usuario, &contUsuarios, &posicao_usuario);
                 logadoUsuario = 1;
             }
         }
-        
-        while (logadoAdm) {
-            int escolha;
-            printf("Escolha uma ação:\n 1- Listar Usuários\n 2- Listar Músicas\n 3- Listar Playlists\n 4- Deslogar\n");
-            scanf("%d", &escolha);
-            while (getchar() != '\n');
 
-            if (escolha == 1) {
+        while (logadoAdm)
+        {
+            int escolha;
+            do
+            {
+                printf("Escolha uma ação:\n 1- Listar Usuários\n 2- Listar Músicas\n 3- Listar Playlists\n  4- Cadastrar Música\n 5- Consultar um Usuário\n 9- Deslogar\n");
+                scanf("%d", &escolha);
+                while (getchar() != '\n')
+                    ;
+            } while (escolha < 1 || escolha > 9);
+
+            if (escolha == 1)
+            {
                 listarUsuarios(usuario, contUsuarios);
-            } else if (escolha == 2) {
+            }
+            else if (escolha == 2)
+            {
                 listarMusicas(musicas, qtdMusicas);
-            } else if (escolha == 3) {
+            }
+            else if (escolha == 3)
+            {
                 listarPlaylists(usuario, contUsuarios, musicas, qtdMusicas);
-            } else if (escolha == 4) {
+            }
+            else if (escolha == 4)
+            {
+                cadastrarMusicas(musicas, &qtdMusicas);
+            }
+            else if (escolha == 5){
+                consultarUsuario(usuario,contUsuarios);
+            }
+            else if (escolha == 9)
+            {
                 logadoAdm = 0;
                 break;
-            } else {
-                printf("Escolha inválida, tente novamente.\n");
             }
         }
 
-        while (logadoUsuario) {
+        while (logadoUsuario)
+        {
             // Placeholder para funcionalidades do usuário
             // Por agora, apenas desloga o usuário imediatamente
             logadoUsuario = 0;
@@ -151,32 +206,39 @@ int main() {
     return 0;
 }
 
-void lerString(char *str, int maxTam) {
+void lerString(char *str, int maxTam)
+{
     fgets(str, maxTam, stdin);
     int tam = strlen(str);
-    if (tam > 0 && str[tam - 1] == '\n') {
+    if (tam > 0 && str[tam - 1] == '\n')
+    {
         str[tam - 1] = '\0';
     }
 }
 
-int loginAdministrador() {
+int loginAdministrador()
+{
     char senha[TAM_SENHA];
     char senhaAdm[TAM_SENHA] = "m@st3r2024";
     printf("-----------LOGIN ADM-----------\n \n");
-    
+
     printf("Informe a senha para acessar como Administrador:\n");
     lerString(senha, TAM_SENHA);
 
-    if (strcmp(senha, senhaAdm) == 0) {
+    if (strcmp(senha, senhaAdm) == 0)
+    {
         printf("Login como Administrador bem-sucedido!\n\n");
         return 1;
-    } else {
+    }
+    else
+    {
         printf("Senha incorreta!\n\n");
         return 0;
     }
 }
 
-int loginUsuario(struct Usuario usuario[], int *contUsuarios, int *posicao) {
+int loginUsuario(struct Usuario usuario[], int *contUsuarios, int *posicao)
+{
     char login[TAM_LOGIN];
     char senha[TAM_SENHA];
     int encontrado = 0;
@@ -187,8 +249,10 @@ int loginUsuario(struct Usuario usuario[], int *contUsuarios, int *posicao) {
     printf("Digite sua senha: ");
     lerString(senha, TAM_SENHA);
 
-    for (int i = 0; i < *contUsuarios; i++) {
-        if (strcmp(login, usuario[i].login) == 0 && strcmp(senha, usuario[i].senha) == 0) {
+    for (int i = 0; i < *contUsuarios; i++)
+    {
+        if (strcmp(login, usuario[i].login) == 0 && strcmp(senha, usuario[i].senha) == 0)
+        {
             encontrado = 1;
             *posicao = i;
             break;
@@ -198,7 +262,8 @@ int loginUsuario(struct Usuario usuario[], int *contUsuarios, int *posicao) {
     return encontrado;
 }
 
-void cadastro(struct Usuario usuario[], int *contUsuarios, int *posicao) {
+void cadastro(struct Usuario usuario[], int *contUsuarios, int *posicao)
+{
     struct Usuario novoUsuario;
     char confirmarSenha[TAM_SENHA];
 
@@ -210,12 +275,14 @@ void cadastro(struct Usuario usuario[], int *contUsuarios, int *posicao) {
     printf("Informe o Login:\n");
     lerString(novoUsuario.login, TAM_LOGIN);
 
-    do {
+    do
+    {
         printf("Informe a Senha:\n");
         lerString(novoUsuario.senha, TAM_SENHA);
         printf("Informe novamente a mesma senha:\n");
         lerString(confirmarSenha, TAM_SENHA);
-        if (strcmp(novoUsuario.senha, confirmarSenha) != 0) {
+        if (strcmp(novoUsuario.senha, confirmarSenha) != 0)
+        {
             printf("Senhas diferentes!!\n");
         }
     } while (strcmp(novoUsuario.senha, confirmarSenha) != 0);
@@ -232,9 +299,11 @@ void cadastro(struct Usuario usuario[], int *contUsuarios, int *posicao) {
     printf("Cadastro realizado com sucesso!\n Conta Cadastrada foi Logada automaticamente.\n");
 }
 
-void listarUsuarios(struct Usuario usuario[], int qtdUsuarios) {
+void listarUsuarios(struct Usuario usuario[], int qtdUsuarios)
+{
     printf("-----------LISTA DE USUÁRIOS-----------\n");
-    for (int i = 0; i < qtdUsuarios; i++) {
+    for (int i = 0; i < qtdUsuarios; i++)
+    {
         printf("Usuário %d:\n", i + 1);
         printf("Nome: %s\n", usuario[i].nome);
         printf("Login: %s\n", usuario[i].login);
@@ -242,17 +311,21 @@ void listarUsuarios(struct Usuario usuario[], int qtdUsuarios) {
     }
 }
 
-void listarPlaylists(struct Usuario usuario[], int qtdUsuarios, struct Musica musicas[], int qtdMusicas) {
+void listarPlaylists(struct Usuario usuario[], int qtdUsuarios, struct Musica musicas[], int qtdMusicas)
+{
     printf("-----------LISTA DE PLAYLISTS-----------\n");
-    for (int i = 0; i < qtdUsuarios; i++) {
-        for (int j = 0; j < usuario[i].qtdPlaylists; j++) {
+    for (int i = 0; i < qtdUsuarios; i++)
+    {
+        for (int j = 0; j < usuario[i].qtdPlaylists; j++)
+        {
             printf("Usuário %d:\n", i + 1);
             printf("Código: %d\n", usuario[i].playlists[j].codigo);
             printf("Título: %s\n", usuario[i].playlists[j].titulo);
             printf("Nome do Criador: %s\n", usuario[i].nome);
             printf("Músicas:\n");
-            for (int k = 0; k < usuario[i].playlists[j].qtdMusicas; k++) {
-                char* titulo = buscarTituloMusica(musicas, qtdMusicas, usuario[i].playlists[j].musicas[k]);
+            for (int k = 0; k < usuario[i].playlists[j].qtdMusicas; k++)
+            {
+                char *titulo = buscarTituloMusica(musicas, qtdMusicas, usuario[i].playlists[j].musicas[k]);
                 printf("%dª- %s\n", k + 1, titulo);
             }
             printf("\n");
@@ -260,21 +333,149 @@ void listarPlaylists(struct Usuario usuario[], int qtdUsuarios, struct Musica mu
     }
 }
 
-char* buscarTituloMusica(struct Musica musicas[], int qtdMusicas, int codigo) {
-    for (int i = 0; i < qtdMusicas; i++) {
-        if (musicas[i].codigo == codigo) {
+char *buscarTituloMusica(struct Musica musicas[], int qtdMusicas, int codigo)
+{
+    for (int i = 0; i < qtdMusicas; i++)
+    {
+        if (musicas[i].codigo == codigo)
+        {
             return musicas[i].titulo;
         }
     }
     return "Música não encontrada";
 }
 
-void listarMusicas(struct Musica musicas[], int qtdMusicas) {
+void listarMusicas(struct Musica musicas[], int qtdMusicas)
+{
     printf("-----------LISTA DE MÚSICAS-----------\n");
-    for (int i = 0; i < qtdMusicas; i++) {
+    for (int i = 0; i < qtdMusicas; i++)
+    {
         printf("----------Música %d----------\n", i + 1);
         printf("Código: %d\n", musicas[i].codigo);
         printf("Título: %s\n", musicas[i].titulo);
         printf("Artista: %s\n", musicas[i].artista);
     }
 }
+
+void cadastrarMusicas(struct Musica musicas[], int *qtdMusicas)
+{
+    int qtds = TAM_MAX_PLYLST;
+    if (*qtdMusicas < qtds)
+    {
+        printf("Informe o Titulo da Música:\n");
+        lerString(musicas[*qtdMusicas].titulo, TAM_NOME);
+        printf("Informe o Nome do Artista:\n ");
+        lerString(musicas[*qtdMusicas].artista, TAM_NOME);
+        musicas[*qtdMusicas].codigo = *qtdMusicas + 1;
+        *qtdMusicas += 1;
+        printf("Música Cadastrada!!\n Código da Música: %d", *qtdMusicas);
+    }
+    else
+    {
+        printf("Não é possível adicionar mais músicas, lista de Músicas Cheia.\n Exclua alguma Música para cadastrar outra Música!!\n");
+    }
+}
+
+void consultarUsuario(struct Usuario usuario[], int qtdUsuarios)
+{
+    int opcao;
+    do
+    {
+        printf("Como deseja buscar o usuário?\n 1- Por código\n 2- Por login\n 3- Por parte do nome\n");
+        scanf("%d", &opcao);
+        while (getchar() != '\n')
+            ;
+    } while (opcao < 1 || opcao > 3);
+
+    int encontrado = 0; // Adicionando uma variável para controlar se o usuário foi encontrado
+
+    if (opcao == 1)
+    {
+        int codigo;
+        printf("Informe o código do usuário: ");
+        scanf("%d", &codigo);
+        while (getchar() != '\n')
+            ;
+
+        for (int i = 0; i < qtdUsuarios; i++)
+        {
+            if (usuario[i].codigo == codigo)
+            {
+                printf("-----------USUÁRIO ENCONTRADO-----------\n");
+                printf("Nome: %s\n", usuario[i].nome);
+                printf("Login: %s\n", usuario[i].login);
+                printf("Código: %d\n", usuario[i].codigo);
+                printf("Quantidade de Playlists: %d\n", usuario[i].qtdPlaylists);
+                printf("Quantidade de Playlists Favoritadas: %d\n", usuario[i].qtdPlaylistsFav);
+                printf("Código das Playlists Favoritadas: ");
+                for (int j = 0; j < usuario[i].qtdPlaylistsFav; j++)
+                {
+                    printf("%d ", usuario[i].playlistsFav[j]);
+                }
+                printf("\n");
+                encontrado = 1;
+                
+            }
+        }
+    }
+    else if (opcao == 2)
+    {
+        char login[TAM_LOGIN];
+        printf("Informe o login do usuário: ");
+        lerString(login, TAM_LOGIN);
+
+        for (int i = 0; i < qtdUsuarios; i++)
+        {
+            if (strcmp(usuario[i].login, login) == 0)
+            {
+                printf("-----------USUÁRIO ENCONTRADO-----------\n");
+                printf("Nome: %s\n", usuario[i].nome);
+                printf("Login: %s\n", usuario[i].login);
+                printf("Código: %d\n", usuario[i].codigo);
+                printf("Quantidade de Playlists: %d\n", usuario[i].qtdPlaylists);
+                printf("Quantidade de Playlists Favoritadas: %d\n", usuario[i].qtdPlaylistsFav);
+                printf("Código das Playlists Favoritadas: ");
+                for (int j = 0; j < usuario[i].qtdPlaylistsFav; j++)
+                {
+                    printf("%d ", usuario[i].playlistsFav[j]);
+                }
+                printf("\n");
+                encontrado = 1;
+                
+            }
+        }
+    }
+    else if (opcao == 3)
+    {
+        char parteNome[TAM_NOME];
+        printf("Informe parte do nome do usuário: ");
+        lerString(parteNome, TAM_NOME);
+
+        for (int i = 0; i < qtdUsuarios; i++)
+        {
+            if (strstr(usuario[i].nome, parteNome) != NULL)
+            {
+                printf("-----------USUÁRIO ENCONTRADO-----------\n");
+                printf("Nome: %s\n", usuario[i].nome);
+                printf("Login: %s\n", usuario[i].login);
+                printf("Código: %d\n", usuario[i].codigo);
+                printf("Quantidade de Playlists: %d\n", usuario[i].qtdPlaylists);
+                printf("Quantidade de Playlists Favoritadas: %d\n", usuario[i].qtdPlaylistsFav);
+                printf("Código das Playlists Favoritadas: ");
+                for (int j = 0; j < usuario[i].qtdPlaylistsFav; j++)
+                {
+                    printf("%d ", usuario[i].playlistsFav[j]);
+                }
+                printf("\n");
+                encontrado = 1;
+
+            }
+        }
+    }
+
+    if (!encontrado)
+    {
+        printf("Usuário não encontrado.\n");
+    }
+}
+
